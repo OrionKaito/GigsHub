90bcef01da09b3ad2a03a5c7499c85a81079e7e8
tandat1405 <33952110+tandat1405@users.noreply.github.com>
2018-11-01 01:01:53 +0700

    Budget + Register Popup + Payment Method

--- Mobile/Gigshub1/app/src/main/java/com/example/myfuckingpc/gigshub1/LoginActivity.java
+++ Mobile/Gigshub1/app/src/main/java/com/example/myfuckingpc/gigshub1/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.myfuckingpc.gigshub1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText et_username, et_password;
-
+    private PopupWindow pw;
+    private EditText edt_register_email, edt_register_username, edt_register_password, edt_register_confirm_password ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,27 @@ public class LoginActivity extends AppCompatActivity {
     public void clickToLogin(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void clickToRegister(View view) {
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        pw = new PopupWindow(inflater.inflate(R.layout.popup_register, null,false), LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT, true);
+        pw.setAnimationStyle(R.style.popup_window_animation_phone);
+        pw.showAtLocation(view, Gravity.CENTER,0,0);
+
+    }
+
+    public void clickToCancelRegister(View view) {
+        pw.dismiss();
+    }
+
+    public void clickToSignUp(View view) {
+        View v = pw.getContentView();
+        edt_register_email = v.findViewById(R.id.edt_register_email);
+        edt_register_username = v.findViewById(R.id.edt_register_username);
+        edt_register_password = v.findViewById(R.id.edt_register_password);
+        edt_register_confirm_password = v.findViewById(R.id.edt_register_confirm_password);
+
+        String email = edt_register_email.getText().toString();
     }
 }
