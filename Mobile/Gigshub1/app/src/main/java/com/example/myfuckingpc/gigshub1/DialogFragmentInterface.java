d52b0511401df25390660d9bbeff2eadd4643015
Trần Đức Thái <dthai9323@gmail.com>
2018-11-10 15:55:00 +0700

    admin page with category

---
+++ Mobile/Gigshub1/app/src/main/java/com/example/myfuckingpc/gigshub1/DialogFragmentInterface.java
@@ -0,0 +1,8 @@
+package com.example.myfuckingpc.gigshub1;
+
+import android.support.v4.app.DialogFragment;
+
+public interface DialogFragmentInterface {
+    public void onDialogPositiveClick(DialogFragment dialog);
+    public void onDialogNegativeClick(DialogFragment dialog);
+}
